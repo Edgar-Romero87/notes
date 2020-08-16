ad39ff2531e09f21ddab2811717f8a3ad725034b
EDGAR ROMERO <EDGAROMERO1987@GMAIL.COM>
2020-08-16 00:19:54 -0700

    worked on schema file

--- __tests__/notes.test.js
+++ __tests__/notes.test.js
@@ -1,78 +1,13 @@
 'use strict';
 
-require ('@code-fellows/supergoose');
-require('../jest.config');
 
-const Notes = require('../libs/notes.js');
+const Notes = require('../libs/notes');
 
-const notes = new Notes();
-jest.spyOn(notes, 'add');
-jest.spyOn(notes, 'list');
-jest.spyOn(notes, 'delete');
-
-describe('Add method', () => {
-
-  it('execute() does nothing with invalid options', () => {
-    return notes.execute({})
-      .then(() => {
-        expect(notes.add).not.toHaveBeenCalled();
-      });
+describe('execute', ()=>{
+  it('execute does nothing with invalid options', () =>{
+    const notes = new Notes({command: 'add', payload: 'test'});
+    return notes.add();
+    expect(notes.add).toHaveBeenCalled();
   });
-  
-  it('notes() can add a note', () => {
-    const action = 'add';
-    const payload = 'test note';
-    return notes.execute({ action, payload })
-      .then(results => {
-        expect(notes.add).toHaveBeenCalled();
-      });
-  });
-  
-  it('notes() can return a saved note', () => {
-    const action = 'add';
-    const payload = 'test note';
-    return notes.execute({ action, payload })
-      .then(savedNote => {
-        expect(savedNote.category).toBe('general');
-        expect(savedNote.text).toBe('test note');
-      });
-  });
-
 });
 
-describe('List method', () => {
-  
-  it('execute() does nothing with invalid options', () => {
-    return notes.execute({})
-      .then(() => {
-        expect(notes.list).not.toHaveBeenCalled();
-      });
-  });
-
-  it('notes() can list notes', () => {
-    const action = 'list';
-    return notes.execute({action})
-      .then(results => {
-        expect(notes.list).toHaveBeenCalled();
-      });
-  });
-
-});
-
-describe('Delete method', () => {
-
-  it('execute() does nothing with invalid options', () => {
-    return notes.execute({})
-      .then(() => {
-        expect(notes.delete).not.toHaveBeenCalled();
-      });
-  });
-  it('notes() can list notes', () => {
-    const action = 'delete';
-    return notes.execute({action})
-      .then(results => {
-        expect(notes.delete).toHaveBeenCalled();
-      });
-  });
-
-});
--- index.js
+++ index.js
@@ -2,10 +2,19 @@
 
 const minimist = require('minimist');
 const Input = require('./lib/input').default;
-const Notes = require('./lib/notes');
+const Note = require('./lib/notes');
+const mongoose = require('mongoose');
+
+//const Note = require('./notes.schema.js');
+
+mongoose.connect('mongodb://localhost:27017/notes', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const input = new Input();
-const notes = new Notes(input);
+const notes = new Note(input);
+
 if(input.valid()){
   notes.execute();
 } else {
